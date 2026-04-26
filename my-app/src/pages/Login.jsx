70844c110d130ae84fb47157bd50d968ced3cb24
vais2004 <vkawale2004@gmail.com>
2026-04-26 22:40:15 +0530

    update feat: wrote login page code

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { BASE_URL } from "../api/Api";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const {login}=useAuth()
+  const { login } = useAuth();
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+    if (token) {
+      login(token);
+      navigate("albums");
+    }
+  }, [searchParams, login, navigate]);
+
+  const handleGoogleLogin = () => {
+    window.location.href = `${BASE_URL}/auth/google`;
+    // window.location.href = `${BASE_URL}/auth/google`;
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="d-flex flex-column align-items-center mt-5">
+      <h2>Welcome to PhotoVault</h2>
+      <button className="btn btn-primary mt-3" onClick={handleGoogleLogin}>
+        Sign in with Google
+      </button>
+    </div>
+  );
 }
