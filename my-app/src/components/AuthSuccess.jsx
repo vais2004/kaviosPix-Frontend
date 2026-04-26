d5978b288cdde216647506160cb02b7ec8f47125
vais2004 <vkawale2004@gmail.com>
2026-04-26 23:37:33 +0530

    update feat: wrote the code for component Auth success simple loader

---
+++ my-app/src/components/AuthSuccess.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function AuthSuccess() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+    if (token) {
+      login(token);
+      navigate("/albums");
+    } else {
+      navigate("/");
+    }
+  }, [searchParams, login, navigate]);
+  return <div>Logging in...</div>;
+}
